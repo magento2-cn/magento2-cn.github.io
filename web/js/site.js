ef3963eafc8615a6a9fa07e72ed8eff746920f04
Bruce Zeng <zengliwei@163.com>
2019-05-31 19:03:43 +0800

    ^ upgrade for mobile

--- web/js/site.js
+++ web/js/site.js
@@ -36,29 +36,63 @@ require( [ 'jquery' ], function( $ ) {
         scrollTop = tmpScrollTop;
     } );
 
-    let touchStartX, touchStartY;
-    $( document ).on( {
-        touchstart: function( evt ) {
-            touchStartX = evt.changedTouches[0].screenX;
-            touchStartY = evt.changedTouches[0].screenY;
-        },
-        touchend: function( evt ) {
-            let dx = evt.changedTouches[0].screenX - touchStartX;
-            let dy = evt.changedTouches[0].screenY - touchStartY;
-            if ( Math.abs( dx ) > Math.abs( dy ) ) {
-                if ( dx > 0 ) {
-                    $( document ).trigger( 'touch_to_right' );
-                } else {
-                    $( document ).trigger( 'touch_to_left' );
-                }
-            } else {
-                if ( dy > 0 ) {
-                    $( document ).trigger( 'touch_to_down' );
-                } else {
-                    $( document ).trigger( 'touch_to_up' );
-                }
+    /**
+     * Add touch events
+     */
+    $.extend( $.event.special, {
+        touch: {
+            setup: function() {
+                let touchStartX, touchStartY, self = $( this );
+                self.on( {
+                    touchstart: function( evt ) {
+                        touchStartX = evt.changedTouches[0].screenX;
+                        touchStartY = evt.changedTouches[0].screenY;
+                    },
+                    touchend: function( evt ) {
+                        let dx = evt.changedTouches[0].screenX - touchStartX;
+                        let dy = evt.changedTouches[0].screenY - touchStartY;
+                        if ( Math.abs( dx ) > Math.abs( dy ) ) {
+                            if ( dx > 0 ) {
+                                self.trigger( 'touchtoright', {type: 'touchtoright'} );
+                            } else {
+                                self.trigger( 'touchtoleft', {type: 'touchtoleft'} );
+                            }
+                        } else {
+                            if ( dy > 0 ) {
+                                self.trigger( 'touchtodown', {type: 'touchtodown'} );
+                            } else {
+                                self.trigger( 'touchtoup', {type: 'touchtoup'} );
+                            }
+                        }
+                    }
+                } );
             }
         }
+    } );
+
+    $.each( {
+        touchtoright: 'touch',
+        touchtoleft: 'touch',
+        touchtodown: 'touch',
+        touchtoup: 'touch'
+    }, function( orig, fix ) {
+        $.event.special[ orig ] = {
+            delegateType: fix,
+            bindType: fix,
+            handle: function( evt, data ) {
+                if ( data.type !== orig ) {
+                    return;
+                }
+
+                let ret, target = this, related = evt.relatedTarget, handleObj = evt.handleObj;
+                if ( !related || (related !== target && !$.contains( target, related )) ) {
+                    evt.type = handleObj.origType;
+                    ret = handleObj.handler.apply( this, arguments );
+                    evt.type = fix;
+                }
+                return ret;
+            }
+        };
     } );
 
 } );
@@ -292,9 +326,12 @@ require( [ 'jquery', 'text!index.json', 'markdown', 'mousewheel', 'progress', 's
                             open_header: function() {
                                 elNavWrapper.removeClass( 'top' );
                             },
-                            touch_to_right: function() {
+                            touchtoright: function() {
                                 elNavWrapper.addClass( 'active' );
                             }
+                        } );
+                        elNav.on( 'touchtoleft', function() {
+                            elNavWrapper.removeClass( 'active' );
                         } );
 
                         elNavWrapper.find( '.bg' ).on( 'click', function() {
