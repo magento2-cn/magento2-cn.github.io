0268e46454dcdca8c881da9033ec26feee88303d
Bruce Zeng <zengliwei@163.com>
2019-06-16 19:39:25 +0800

    ^ small changes

--- web/js/site.js
+++ web/js/site.js
@@ -230,13 +230,22 @@ require( [ 'jquery', 'text!index.json', 'markdown', 'mousewheel', 'progress', 's
             let elInfo = elBody.find( '> .info' );
             let boxH = elInfo.outerHeight();
             let dyContent = boxH * .8;
-            let dyBg = boxH * .2;
+            let dyBg = boxH * .05;
             let percentage = scrollTop / boxH;
             elInfo.find( '.content .box' ).css( {
                 marginTop: -30 - dyContent * percentage,
                 opacity: 1 - percentage
             } );
             elInfo.find( '.bg' ).css( 'marginTop', -dyBg * percentage );
+
+            let imgH = boxH + dyBg * percentage;
+            let width = imgH * elInfo.find( '.bg img' ).get( 0 ).naturalWidth / elInfo.find( '.bg img' ).get( 0 ).naturalHeight;
+            elInfo.find( '.bg img' ).css( {
+                height: imgH,
+                marginLeft: ( elWin.width() - width ) / 2,
+                width: width
+            } );
+
         }
 
     };
